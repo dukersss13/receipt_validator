fb8bd313488c6503158c1117eb3db64cee1b6b6e
Duc Le <d.le@sap.com>
2024-09-02 16:17:28 -0700

    created DataReader + Validator classes

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from src.data_reader import DataReader, DataType
+from validator import Validator
 
+
+dr = DataReader()
+transactions = dr.read_data(DataType.TRANSACTIONS)
+proofs = dr.read_data(DataType.PROOFS)
+
+validator = Validator(transactions, proofs)
--- src/validator.py
+++ src/validator.py
@@ -3,25 +3,35 @@ from fuzzywuzzy import process, fuzz
 
 
 class Validator:
-    def __init__(self, bank_statement: pd.DataFrame, receipts: pd.DataFrame):
-        self.bank_statement = bank_statement
-        self.receipts = receipts
+    def __init__(self, transactions: pd.DataFrame, proofs: pd.DataFrame):
+        self.transactions = transactions
+        self.proofs = proofs
     
     @staticmethod
-    def match_business_names(transaction_name, receipts_names, threshold=80):
-        match, score = process.extractOne(transaction_name, receipts_names, scorer=fuzz.partial_ratio)
+    def match_business_names(transaction_name: str, proofs: list[str], threshold=80):
+        """
+        Given a list of business names from receipts,
+        match the transaction to the closest name from the receipts.
+
+        :param transaction_name: name of transaction to match
+        :param receipts_names: business names from the receipts
+        :param threshold: threshold of similiarity, defaults to 80
+        :return: the match found
+        """
+        match, score = process.extractOne(transaction_name, proofs, scorer=fuzz.partial_ratio)
+
         return match if score >= threshold else None
 
     def validate(self) -> tuple[pd.DataFrame, pd.DataFrame]:
         # Add a new column in df_bank for the best match from df_receipts
-        self.bank_statement["matched_name"] = self.bank_statement["business_name"].apply(lambda x: \
-                                                                 self.match_business_names(x, self.receipts["business_name"].values))
+        self.transactions["matched_name"] = self.transactions["business_name"].apply(lambda x: \
+                                                              self.match_business_names(x, self.proofs["business_name"].values))
 
-        self.bank_statement["matched_date"] = self.bank_statement["date"].apply(lambda x: \
-                                              self.match_business_names(x, self.receipts["date"].values))
+        self.transactions["matched_date"] = self.transactions["date"].apply(lambda x: \
+                                            self.match_business_names(x, self.proofs["date"].values))
 
         # Merge based on matched names and totals
-        merged_df = self.bank_statement.merge(self.receipts, left_on=["matched_name", "matched_date"], 
+        merged_df = self.transactions.merge(self.proofs, left_on=["matched_name", "matched_date"], 
                                 right_on=["business_name", "date"], how="inner", suffixes=("_bank", "_receipt"))
 
         merged_df = merged_df.drop(columns=["matched_name", "matched_date"])
@@ -34,10 +44,8 @@ class Validator:
     
     @staticmethod
     def find_discrepancies(merged_df: pd.DataFrame) -> pd.DataFrame:
-        """_summary_
-
-        :param merged_df: _description_
-        :return: _description_
+        """
+        Find any discrepancy in the list of transactions
         """
         merged_df["delta"] = abs(merged_df["total_receipt"] - merged_df["total_bank"])
 
@@ -47,7 +55,9 @@ class Validator:
         return discrepancies
 
     def find_unmatched_transactions(self, merged_df: pd.DataFrame) -> pd.DataFrame:
-        # Identify unmatched rows in df_bank
-        unmatched = self.bank_statement[~self.bank_statement["business_name"].isin(merged_df["business_name_bank"])]
+        """
+        Identify any transaction that had no match
+        """
+        unmatched = self.transactions[~self.transactions["business_name"].isin(merged_df["business_name_bank"])]
 
         return unmatched
