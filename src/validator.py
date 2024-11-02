8e10a966757fe3b84a27db97fd9661ea3d89a67a
Duc <dukerss13@yahoo.com>
2024-11-02 22:19:39 +0000

    Finished recommendations df - need to output to UI

--- src/validator.py
+++ src/validator.py
@@ -1,4 +1,3 @@
-from typing import Union
 import numpy as np
 import pandas as pd
 from time import time
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -33,5 +33,25 @@ def test_similar_business_names():
                             "date": ["2023-01-01", "2021-10-12"]})
 
     validator = Validator(transactions, proofs)
+
+    discrepancies, unmatched_transactions, unmatched_proofs = validator.validate()
+
+    assert len(discrepancies)
+    assert not len(unmatched_transactions)
+    assert not len(unmatched_proofs)
+
+
+def test_similar_business_names():
+    # Test validation when there are similar business names
+    transactions = pd.DataFrame({"business_name": ["Ikkousha Craft Ramen", "Taco Bell"],
+                                 "total": [12.30, 14.50],
+                                 "date": ["2023-01-01", "2021-10-12"]})
+    proofs = pd.DataFrame({"business_name": ["Ikkousha Ramen Costa Mesa", "Taco Bell"],
+                            "total": [12.30, 14.50],
+                            "date": ["2023-01-01", "2021-10-12"]})
+
+    validator = Validator(transactions, proofs)
     results = validator.validate()
     recommendations = validator.analyze_results(results)
+
+    assert len(recommendations)
