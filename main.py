1aebb23333c5b71ac19404a4929f4f7f64a425f8
name <email@yahoo.com>
2025-03-30 03:06:59 +0000

    update

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ from src.interface import Interface
 if __name__ == "__main__":
     interface = Interface()
     interface.run_interface()
-
-# TODO
-# Enable PDF read
-# Create a new prompt to handle PDF information extraction
-# Do PDF info extract using vector embeddings / RAG
